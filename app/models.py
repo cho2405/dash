25c0fdb07d2246ee7907014f79a849df4d4939eb
Hana <cho2405@gmail.com>
2021-08-24 13:17:22 +0900

    map html return from httpresponse

--- app/models.py
+++ app/models.py
@@ -1,10 +1,19 @@
 # -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Trading(models.Model):
+    GU_CODE = models.IntegerField(verbose_name='지역코드')
+    DONG_NAME = models.CharField(max_length=256, verbose_name='법정동')
+    TRADE_DATE = models.CharField(max_length=32, verbose_name='거래일')
+    APT_NAME = models.CharField(max_length=256, verbose_name='아파트')
+    JI_NUM = models.CharField(max_length=64, verbose_name='지번') 
+    AREA_SIZE = models.FloatField(verbose_name='전용면적') 
+    FLOOR = models.IntegerField(verbose_name='층')
+    BUILT_YEAR = models.IntegerField(verbose_name='건축년도')
+    TRADE_PRICE = models.IntegerField(verbose_name='거래금액')
+    TRADE_MONTH = models.IntegerField(verbose_name='거래월')
+
 
--- app/views.py
+++ app/views.py
@@ -8,6 +8,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 from django.http import HttpResponse
 from django import template
+from django.db.models import Count
+
+from .models import Trading
 
 @login_required(login_url="/login/")
 def index(request):
@@ -17,6 +20,17 @@ def index(request):
 
     html_template = loader.get_template( 'index.html' )
     return HttpResponse(html_template.render(context, request))
+
+
+def trades_json(request, context):
+    print("!!!")
+
+    gu_trades = Trading.objects.values('GU_CODE','TRADE_MONTH').annotate(cnt=Count('TRADE_MONTH')).order_by()
+    print(gu_trades)
+    
+    context['gu_trades'] = gu_trades
+    return render(request, 'maps-jqvmap.html', context)
+
 
 @login_required(login_url="/login/")
 def pages(request):
@@ -28,6 +42,11 @@ def pages(request):
         load_template      = request.path.split('/')[-1]
         context['segment'] = load_template
         
+        
+        if load_template == "maps-jqvmap.html":
+            print(load_template)
+            return HttpResponse(trades_json(request, context))
+            
         html_template = loader.get_template( load_template )
         return HttpResponse(html_template.render(context, request))
         
@@ -40,3 +59,6 @@ def pages(request):
     
         html_template = loader.get_template( 'page-500.html' )
         return HttpResponse(html_template.render(context, request))
+
+    
+    
